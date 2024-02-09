b09569939be861e3312588bf56a5942e0e882162
mshajahan6300059917@gmail.com <mshajahan6300059917@gmail.com>
2024-02-09 12:53:35 +0530

    program 1 java

---
+++ programe1.java
@@ -0,0 +1,16 @@
+package codej;
+
+public class programe1 {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=2;
+System.out.println("addition="+(a+b));
+System.out.println("subtraction="+(a-b));
+System.out.println("multiplication="+(a*b));
+System.out.println("division="+(a/b));
+System.out.println("modular="+(a%b));
+
+}
+	
+}
